cb1b7af8fc174d5701f85f191c3811c7490079d8
Sherzod Nimatullo <shermanjones99@gmail.com>
2020-02-15 10:56:27 -0500

    updated features

--- classifying.py
+++ classifying.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 features = ['pts_per_g', 'ast_per_g', 'trb_per_g', 'stl_per_g',
-            'mp_per_g', 'ws', 'fg_pct', 'win_pct', 'usg_pct']
+            'mp_per_g', 'ws', 'ts', 'win_pct', 'usg_pct', 'bpm']
 
 df = (pd.read_csv("mvp_votings.csv"))[features]
