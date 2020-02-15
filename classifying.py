44081f5b7ad745e924d806685a608480070a3887
Sherzod Nimatullo <shermanjones99@gmail.com>
2020-02-15 14:34:57 -0500

    gradient boosting regressor

--- classifying.py
+++ classifying.py
@@ -1,13 +1,11 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import numpy as np
 
-features = ['fga', 'fg3a', 'fta', 'per', 'ts_pct', 'usg_pct', 'bpm',
-            'mp_per_g', 'pts_per_g', 'trb_per_g', 'ast_per_g',
-            'stl_per_g', 'blk_per_g', 'fg_pct', 'fg3_pct', 'ft_pct', 'ws',
-            'ws_per_48', 'win_pct']
+features = ['pts_per_g', 'ast_per_g', 'trb_per_g', 'stl_per_g',
+            'mp_per_g', 'blk_per_g', 'ws', 'ts_pct', 'win_pct']
 
 df = (pd.read_csv("mvp_votings.csv"))
 current_players = pd.read_csv("test_data.csv")
@@ -19,7 +17,7 @@ y_train = df['award_share'].to_numpy()
 
 X_train, y_train = shuffle(X_train, y_train)
 
-forest = RandomForestRegressor()
+forest = GradientBoostingRegressor()
 forest.fit(X_train, y_train)
 
 prediction = forest.predict(current_players[features])
@@ -31,6 +29,6 @@ for award_share in prediction:
     index = index + 1
 
 index = 1
-for player in formated_pred:
+for player in sorted(formated_pred, reverse=True):
     print(f'{index}. {formated_pred[player]}: {player}')
     index = index + 1
