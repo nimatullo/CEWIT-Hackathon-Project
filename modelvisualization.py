3d6d33537761739e7e06ae6a2fbae84c612a2a93
William Krasnov <willkoman@gmail.com>
2020-02-15 18:04:34 -0500

    Jointplot with graphs commented for use

--- modelvisualization.py
+++ modelvisualization.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
 tips = pd.read_csv("mvp_votings.csv")
-#sns.relplot(x="award_share", y="stl_per_g", data=tips)
-#sns.relplot(x="award_share", y="ws", data=tips)
-# sns.regplot(x="award_share", y="stl_per_g", data=tips)
-sns.jointplot(x="award_share", y="ws", data=tips,kind="reg")
+# sns.jointplot(x="award_share", y="stl_per_g", data=tips,kind="reg",color="m")
+sns.jointplot(x="award_share", y="ws", data=tips,kind="reg",color="m")
 
+# Grid Graphs=========================================================
+# features = ['pts_per_g', 'ast_per_g', 'trb_per_g', 'stl_per_g',
+#             'mp_per_g', 'blk_per_g', 'ws', 'ts_pct', 'win_pct']
+# x_data = ['award_share','ws']
+# sns.pairplot(tips,x_vars=x_data,y_vars=features,kind="reg")
+#=====================================================================
 plt.show()
