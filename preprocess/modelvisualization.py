ae34fb1d3f1da2ab08f0323d04c0c268aec8cdf4
William Krasnov <willkoman@gmail.com>
2020-02-15 19:08:06 -0500

    Fixed Spreadsheet Location

--- preprocess/modelvisualization.py
+++ preprocess/modelvisualization.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
-tips = pd.read_csv("mvp_votings.csv")
+tips = pd.read_csv("data/mvp_votings.csv")
 # sns.jointplot(x="award_share", y="stl_per_g", data=tips,kind="reg",color="m")
 sns.jointplot(x="award_share", y="ws", data=tips,kind="reg",color="m")
 
